c5080f5e79d69313b194c49fea6ee7da2783ea2e
RAFazarikha <rafazarikha24@gmail.com>
2025-01-07 19:17:28 +0700

    first commit

---
+++ mail/contact.php
@@ -0,0 +1,27 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "fazarikha923@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+
+if(!mail($to, $subject, $body, $header)) {
+  error_log("Mail failed to send to $to");
+  http_response_code(500);
+}
+
+?>
